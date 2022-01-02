e0a1753fec223c0ed09d55207bf5c94e092e7a0c
YaleFeng <42980368+YaleFeng@users.noreply.github.com>
2022-01-02 14:46:36 -0700

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,11 @@
-# run_analysis.R
-
-#0. prepare LIBs
 library(reshape2)
 
-
-#1. get dataset from web
-rawDataDir <- "./rawData"
-rawDataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-rawDataFilename <- "rawData.zip"
-rawDataDFn <- paste(rawDataDir, "/", "rawData.zip", sep = "")
-dataDir <- "./data"
+# Get dataset
+rawDataDir = "./RawData"
+rawDataUrl = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+rawDataFilename = "rawData.zip"
+rawDataDFn = paste(rawDataDir, "/", rawDataFilename, sep = "")
+dataDir = "./Data"
 
 if (!file.exists(rawDataDir)) {
   dir.create(rawDataDir)
@@ -20,53 +16,48 @@ if (!file.exists(dataDir)) {
   unzip(zipfile = rawDataDFn, exdir = dataDir)
 }
 
-
-#2. merge {train, test} data set
-# refer: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+# Merge data
+path_rf <- file.path(dataDir , "UCI HAR Dataset")
 # train data
-x_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/X_train.txt"))
-y_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/Y_train.txt"))
-s_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/subject_train.txt"))
+x_train = read.table(file.path(path_rf, "train" , "X_train.txt" ),header = FALSE)
+y_train = read.table(file.path(path_rf, "train" , "Y_train.txt" ),header = FALSE)
+s_train = read.table(file.path(path_rf, "train" , "subject_train.txt" ),header = FALSE)
 
 # test data
-x_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/X_test.txt"))
-y_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/Y_test.txt"))
-s_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/subject_test.txt"))
+x_test = read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+y_test = read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+s_test = read.table(file.path(path_rf, "test" , "subject_test.txt" ),header = FALSE)
 
-# merge {train, test} data
-x_data <- rbind(x_train, x_test)
-y_data <- rbind(y_train, y_test)
-s_data <- rbind(s_train, s_test)
-
-
-#3. load feature & activity info
-# feature info
-feature <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/features.txt"))
-
-# activity labels
-a_label <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/activity_labels.txt"))
-a_label[,2] <- as.character(a_label[,2])
-
-# extract feature cols & names named 'mean, std'
-selectedCols <- grep("-(mean|std).*", as.character(feature[,2]))
-selectedColNames <- feature[selectedCols, 2]
-selectedColNames <- gsub("-mean", "Mean", selectedColNames)
-selectedColNames <- gsub("-std", "Std", selectedColNames)
-selectedColNames <- gsub("[-()]", "", selectedColNames)
+# merge train, test together
+x_data = rbind(x_train, x_test)
+y_data = rbind(y_train, y_test)
+s_data = rbind(s_train, s_test)
 
 
-#4. extract data by cols & using descriptive name
-x_data <- x_data[selectedCols]
-allData <- cbind(s_data, y_data, x_data)
-colnames(allData) <- c("Subject", "Activity", selectedColNames)
+# Load information
 
-allData$Activity <- factor(allData$Activity, levels = a_label[,1], labels = a_label[,2])
-allData$Subject <- as.factor(allData$Subject)
+feature = read.table(file.path(path_rf, "features.txt" ),header = FALSE)
 
+a_label = read.table(file.path(path_rf, "activity_labels.txt" ),header = FALSE)
+a_label[,2] = as.character(a_label[,2])
 
-#5. generate tidy data set
-meltedData <- melt(allData, id = c("Subject", "Activity"))
-tidyData <- dcast(meltedData, Subject + Activity ~ variable, mean)
+cols = grep("-(mean|std).*", as.character(feature[,2]))
+colNames = feature[cols, 2]
+colNames = gsub("-mean", "Mean", colNames)
+colNames = gsub("-std", "Std", colNames)
+colNames = gsub("[-()]", "", colNames)
 
-write.table(tidyData, "./tidy_dataset.txt", row.names = FALSE, quote = FALSE)
+# Merge
+x_data = x_data[selectedCols]
+all_Data = cbind(s_data, y_data, x_data)
+colnames(all_Data) = c("Subject", "Activity", colNames)
+
+all_Data$Activity = factor(all_Data$Activity, levels = a_label[,1], labels = a_label[,2])
+all_Data$Subject = as.factor(all_Data$Subject)
+
+# Write tidy data
+melted_Data = melt(all_Data, id = c("Subject", "Activity"))
+tidy_Data = dcast(melted_Data, Subject + Activity ~ variable, mean)
+
+write.table(tidy_Data, "./tidy_dataset.txt", row.names = FALSE, quote = FALSE)
 
